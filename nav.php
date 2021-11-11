d8cc76e5211b0cab21c95c4e0ba18230bb675b2b
Shane <shaneguignard@gmail.com>
2021-11-11 12:28:01 -0500

    first commit

---
+++ nav.php
@@ -0,0 +1,14 @@
+
+<nav>
+    <li onclick="menu(false); pages('home');">HOME</li>
+    <li onclick="menu(false); pages('websites');">WEBSITES</li>
+    <!-- <li onclick="menu(false); pages('blog');">BLOG</li> -->
+    <li onclick="menu(false); pages('webapps');">WEBAPPS</li>
+    <li onclick="menu(false); pages('resume');">RESUME</li>
+    <!--            <li onclick="menu(false); pages('businesses');">BUSINESSES</li>-->
+    <!--            <li onclick="menu(false); pages('music');">MUSIC</li>-->
+    <!--            <li onclick="menu(false); pages('technology');">TECHNOLOGY</li>-->
+    <!--            <li onclick="menu(false); pages('food');">FOOD</li>-->
+    <li onclick="menu(false); pages('research');">RESEARCH</li>
+    <li onclick="menu(false); pages('about');">ABOUT</li>
+</nav>
